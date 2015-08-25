eecea5800f29b8beb2664807186d86eb87a078b5
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2015-08-25 21:49:37 +0100

    Queue: add default ctor and enqueue stub.

--- include/Queue.h
+++ include/Queue.h
@@ -1,8 +1,47 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
+#include <cstddef>
+
+/* The Queue data structure
+ * Public interface:
+ * enqueue()
+ * dequeue()
+ * size()
+ * clear()
+ * begin()
+ * end() : one pass the last item
+ * empty()
+ */
 
 template <typename Element>
 class Queue {
+ public:
+  // default Ctor
+  Queue() : size_{0}, head_{nullptr}, tail_{nullptr} {}
+
+  // the public interface:
+  size_t size() {
+    return size_;
+  }
+
+  void enqueue(const Element& element) {
+  }
+
+ private:
+  // A queue should contains a pointer to the first node,
+  // a pointer to the last node and size
+  struct Node {
+    // NOT YET
+  };
+
+  size_t size_;
+  Node* head_;
+  Node* tail_;
+
+  // you must disable the option of const Queue.
+
 };
+
+
 
 #endif // QUEUE_H_
