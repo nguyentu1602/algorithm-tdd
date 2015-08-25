198d036f4d021e657bcfd896358f8607a7766187
Cuong Nguyen <cuong.nguyentumanh@gmail.com>
2015-08-25 22:54:17 +0100

    Queue: implement enqueue.

--- include/Queue.h
+++ include/Queue.h
@@ -20,25 +20,45 @@ class Queue {
   Queue() : size_{0}, head_{nullptr}, tail_{nullptr} {}
 
   // the public interface:
-  size_t size() {
+  size_t size() const {
     return size_;
   }
 
-  void enqueue(const Element& element) {
+  bool empty() const {
+    return size_ == 0;
   }
+
+  void enqueue(const Element& element) {
+    Node* new_node = new Node(element);
+    if (empty()) {
+      head_ = tail_ = new_node;
+    }
+    else {
+      tail_->next_ = new_node;
+      tail_ = new_node;
+    }
+    size_++;
+ }
 
  private:
   // A queue should contains a pointer to the first node,
   // a pointer to the last node and size
+
+  // A Node in the queue - implemented as item in a singly-linked list
   struct Node {
-    // NOT YET
+    Element element_;
+    Node* next_;
+    explicit Node(const Element& element) : element_{element}, next_{nullptr} {}
+
+    // disable the default copy ctor and assignment operator:
+    const Node& operator=(const Node&);
+    Node(const Node&);
   };
 
   size_t size_;
   Node* head_;
   Node* tail_;
 
-  // you must disable the option of const Queue.
 
 };
 
